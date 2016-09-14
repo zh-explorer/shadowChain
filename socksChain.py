aa698b917d8a02437e6f060159c3d0c6b0da9a00
explorer <hsadkhk@gmail.com>
2016-09-14 18:53:39 +0800

    rebuild

--- socksChain.py
+++ socksChain.py
@@ -39,6 +39,77 @@ class shadowChain(object):
             addrtype = self.getAddrType(addr)
             self.iplist.append((addr, int(port), addrtype))
 
+        iplist = list(self.iplist)
+
+        while True:
+            self.uselist = []
+            remote = self.testConnect(iplist)
+            if remote == None:
+                print "can't find"
+                exit(1)
+            while True:
+                if len(iplist) == 0:
+                    try:
+                        remote.send('\x05\x01\x00')
+                        if remote.recv(2) == '\x05\x00':
+                            self.uselist.append(self.last)
+                    except:
+                        pass
+                    if len(self.uselist) == 0:
+                        print "can't find"
+                        exit(1)
+                    self.iplist = self.uselist
+                    print len(self.iplist)
+                    print self.iplist
+                    return
+                addr, port, addrtype = iplist.pop(0)
+                if self.testSock(remote, addr, port, addrtype) == False:
+                    iplist = self.uselist + iplist
+                    break
+
+    def testSock(self, remote, addr, port, addrtype):
+        try:
+            remote.send('\x05\x01\x00')
+            if remote.recv(2) == '\x05\x00':
+                self.uselist.append(self.last)
+                print "try sock " + addr + ':' + str(port)
+                if addrtype == 1:
+                    data = '\x05\x01\x00\x01' + socket.inet_aton(addr) + struct.pack(">H", port)
+                elif addrtype == 3:
+                    data = '\x05\x01\x00\x03' + chr(len(addr)) + addr + struct.pack(">H", port)
+                remote.send(data)
+                data = remote.recv(4)
+
+                if data[1] != '\x00':
+                    return False
+                else:
+                    if data[3] == '\x01':
+                        remote.recv(4+2)
+                    elif data[3] == '\x03':
+                        remote.recv(ord(remote.recv(1)[0])+2)
+                    self.last = addr, port, addrtype
+                    return True
+        except:
+            return False
+
+    def testConnect(self, iplist):
+        while True:
+            try:
+                remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                t = len(self.uselist)+2
+                t /= 2
+                remote.settimeout(t)
+                if len(iplist) == 0:
+                    return None
+                addr, port, addrtype = iplist.pop(0)
+                self.last = addr, port, addrtype
+                print "try connect " + addr + ':' + str(port)
+                remote.connect((addr, port))
+                break
+            except:
+                pass
+        return remote
+
     def sockNext(self, sock, iplist):
         sock.send('\x05\x01\x00')
         if sock.recv(2) == '\x05\x00':
